94869b1a644bb6a6f1cf149b74783a462faa58c2
nivashinirajann <nivashinir@jfrog.com>
2026-05-28 17:12:33 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='frogbot-oidc-test',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.28.0',
+        'flask==2.0.1',
+        'Werkzeug==2.0.1',
+        'PyYAML==5.3.1',
+        'Pillow==9.0.0',
+    ],
+)
